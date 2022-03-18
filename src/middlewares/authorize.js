5d8b33e0a2ad9c919f44d53bf78ddbcda825b8d6
Adjaro Ogaga <ogagaadjaro@gmail.com>
2022-03-18 11:50:41 +0100

    feat-Added controllers

---
+++ src/middlewares/authorize.js
@@ -0,0 +1,60 @@
+const { HTTP } = require("../constants/http");
+const { RESPONSE } = require("../constants/response");
+const createError = require("../helpers/createError");
+const { jwtVerify } = require("../helpers/token");
+const User = require("../app/modules/user/repository/user.repository");
+
+
+exports.checkAuth = async (req, _, next) => {
+  const message = "Unauthorized";
+  const token =
+    req.headers["authorization"] && req.headers["authorization"].split(" ")[1];
+
+  if (!token) {
+    return next(
+      createError(HTTP.UNAUTHORIZED, [
+        {
+          status: RESPONSE.ERROR,
+          message,
+          statusCode: HTTP.UNAUTHORIZED,
+        },
+      ])
+    );
+  }
+  try {
+    const { id } = jwtVerify(token) || {};
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      return next(
+        createError(HTTP.UNAUTHORIZED, [
+          {
+            status: RESPONSE.ERROR,
+            message,
+            statusCode: HTTP.UNAUTHORIZED,
+          },
+        ])
+      );
+    }
+    if (user) {
+      req.userId = id;
+      req.user = user;
+      req.token = token;
+      return next();
+    }
+
+    return next(
+      createError(HTTP.UNAUTHORIZED, [
+        {
+          status: RESPONSE.ERROR,
+          message,
+          statusCode: HTTP.UNAUTHORIZED,
+        },
+      ])
+    );
+  } catch (err) {
+    console.log(err);
+    return next(createError.InternalServerError());
+  }
+};
